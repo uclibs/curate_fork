6d3936e1fe5d418389bf9897dcba1b9dcf422e0e
James Van Mil <james.vanmil@gmail.com>
2015-11-06 12:56:06 -0500

    refactor with_editors concern to abstract functionality

--- app/repository_models/curation_concern/with_editors.rb
+++ app/repository_models/curation_concern/with_editors.rb
@@ -7,94 +7,163 @@ module CurationConcern
     end
 
     def add_editor_group(group)
-      raise ArgumentError, "parameter is #{group.inspect}, expected a kind of Hydramata::Group" unless group.is_a?(Hydramata::Group)
-      editor_groups << group
-      self.permissions_attributes = [{ name: group.pid, access: 'edit',
-                                       type: 'group' }]
-      self.save!
-      group.works << self
-      group.save!
+      add_group(group, :edit)
     end
 
     # @param groups [Array<Hydramata::Group>] a list of groups to add 
     def add_editor_groups(groups)
-      groups.each do |g|
-        add_editor_group(g)
-      end
+      add_groups(groups, :edit)
     end
 
     def remove_editor_group(group)
-      return unless edit_groups.include?(group.pid)
-      editor_groups.delete(group)
-      self.edit_groups = edit_groups - [group.pid]
-      self.save!
-      group.works.delete(self)
-      group.save!
+      remove_group(group, :edit)
     end
 
     # @param groups [Array<Hydramata::Group>] a list of users to remove
     def remove_editor_groups(groups)
-      groups.each do |g|
-        remove_editor_group(g)
-      end
+      remove_groups(groups, :edit)
     end
 
     # @param user [User] the user account you want to grant edit access to.
     def add_editor(user)
-      raise ArgumentError, "parameter is #{user.inspect}, expected a kind of User" unless user.is_a?(User)
-      editors << user.person
-      self.permissions_attributes = [{ name: user.user_key, access: 'edit', type: 'person' }] unless depositor == user.user_key
+      add_user(user, :edit)
     end
 
     # @param users [Array<User>] a list of users to add
     def add_editors(users)
-      users.each do |u|
-        add_editor(u)
-      end
+      add_users(users, :edit)
     end
 
     # @param user [User] the user account you want to revoke edit access for.
     def remove_editor(user)
-      remove_candidate_editor(user) if can_remove_editor?(user)
+      remove_user(user, :edit)
     end
 
     # @param users [Array<User>] a list of users to remove
     def remove_editors(users)
-      users.each do |u|
-        remove_editor(u)
-      end
+      remove_users(users, :edit)
     end
 
     private
 
-      # Decide if the user can be removed as an editor.  They cannot be removed
-      # if they are the depositor or if they are not presently an editor
-      # @param user [User] the user to remove
-      def can_remove_editor?(user)
-        depositor != user.user_key && editors.include?(user.person)
+    def add_group(group, access_type)
+      raise ArgumentError, "parameter is #{group.inspect}, expected a kind of Hydramata::Group" unless group.is_a?(Hydramata::Group)
+
+      if access_type == :edit
+        editor_groups << group
+        self.permissions_attributes = [{ name: group.pid, access: 'edit', type: 'group' }]
+      else
+        access_type_error
       end
 
-      def clear_associations
-        clear_editor_groups
-        clear_editors
-      end
+      # TODO extent to read groups
+      group.works << self
+      group.save!
+    end
 
-      def clear_editor_groups
-        editor_groups.each do |editor_group|
-          remove_editor_group(editor_group)
+    def add_groups(groups, access_type)
+      if access_type == :edit
+        groups.each do |g|
+          add_editor_group(g)
         end
+      else
+        access_type_error
       end
+    end
 
-      def clear_editors
-        editors.each do |editor|
-          remove_candidate_editor(User.find_by_repository_id(editor.pid))
+    def remove_group(group, access_type)
+      if access_type == :edit
+        return unless edit_groups.include?(group.pid)
+        editor_groups.delete(group)
+        self.edit_groups = edit_groups - [group.pid]
+        self.save!
+        group.works.delete(self)
+        group.save!
+      else
+        access_type_error
+      end
+    end
+
+    def remove_groups(groups, access_type)
+      if access_type == :edit
+        groups.each do |g|
+          remove_editor_group(g)
         end
+      else
+        access_type_error
       end
+    end
 
-      def remove_candidate_editor(user)
-        editors.delete(user.person)
-        self.edit_users = edit_users - [user.user_key]
-        user.person.works.delete(self)
+    def add_user(user, access_type)
+      raise ArgumentError, "parameter is #{user.inspect}, expected a kind of User" unless user.is_a?(User)
+      if access_type == :edit
+        editors << user.person
+        self.permissions_attributes = [{ name: user.user_key, access: 'edit', type: 'person' }] unless depositor == user.user_key
+      else
+        access_type_error
       end
+    end
+
+    def add_users(users, access_type)
+      if access_type == :edit
+        users.each do |u|
+          add_editor(u)
+        end
+      else
+        access_type_error
+      end
+    end
+
+    def remove_user(user, access_type)
+      if access_type == :edit
+        remove_candidate_editor(user) if can_remove_editor?(user)
+      else
+        access_type_error
+      end
+    end
+
+    def remove_users(users, access_type)
+      if access_type == :edit
+        users.each do |u|
+          remove_editor(u)
+        end
+      else
+        access_type_error
+      end
+    end
+    
+    def access_type_error
+      raise ArgumentError, "parameter #{access_type} is not a valid access type"
+    end
+
+    # Decide if the user can be removed as an editor.  They cannot be removed
+    # if they are the depositor or if they are not presently an editor
+    # @param user [User] the user to remove
+    def can_remove_editor?(user)
+      depositor != user.user_key && editors.include?(user.person)
+    end
+
+    def clear_associations
+      clear_editor_groups
+      clear_editors
+    end
+
+    def clear_editor_groups
+      editor_groups.each do |editor_group|
+        remove_editor_group(editor_group)
+      end
+    end
+
+    def clear_editors
+      editors.each do |editor|
+        remove_candidate_editor(User.find_by_repository_id(editor.pid))
+      end
+    end
+
+    def remove_candidate_editor(user)
+      editors.delete(user.person)
+      self.edit_users = edit_users - [user.user_key]
+      user.person.works.delete(self)
+    end
   end
 end
